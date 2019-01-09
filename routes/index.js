419c3f1fc5b2bfdf9ba3d7aa9321a2cbec3a3abd
caue <nncl@live.com>
2019-01-09 10:41:52 -0200

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var prerender = require('prerender-node');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Metatags' });
+});
+
+router.get('/routes', function(req, res, next) {
+
+    console.log(prerender);
+
+    res.send('ok ok');
 });
 
 module.exports = router;
